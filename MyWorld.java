435f05f41f1aaa4572000d3bfc00113c347918dc
WonTonnn <wongruotong@gmail.com>
2024-01-09 09:17:24 -0500

    Added fight system things, working on buttons

--- MyWorld.java
+++ MyWorld.java
@@ -27,21 +27,46 @@ public class MyWorld extends World
     BlueSplash choiceBlue = new BlueSplash();
     public int length = 100;
     public int width = 100;
+    
+    public int buttonHeight = 50;
+    public int buttonWidth = 200;
+    
     public boolean fighting = false;
     public boolean madeChoice = false;
     public int defaultSize = 70;
     SimpleTimer introTimer = new SimpleTimer();
     FightPlace fightWorld = new FightPlace();
-    
+
+    SimpleTimer turnTimer = new SimpleTimer();
+
+    AttackButton attackButton = new AttackButton();
+    BlockButton blockButton = new BlockButton();
+    SkillButton skillButton = new SkillButton();
+
+    //Create all bar related stuff
     public int healthMax = 100;
-    
+
     public int health = 100;
     Label healthBar;
+    Color healthColor = new Color(247, 22, 22);
     public int block = 100;
     Label blockBar;
+    Color blockColor = new Color(36, 181, 77);
     public int mana = 100;
     Label manaBar;
-        
+    Color manaColor = new Color(22, 168, 247);
+
+    public int turnDecision = 0;
+    /*
+     * 1 = attack
+     * 2 = skill
+     * 3 = block
+     * 4 = ultimate
+     * 0 = nothing
+     */
+    
+    public boolean myTurn;
+
     EnemyOne enemy1 = new EnemyOne();
 
     public MyWorld()
@@ -50,30 +75,82 @@ public class MyWorld extends World
         super(600, 400, 1);
         createBat();
         addChoice();     
-        
+        createBars();
+
+    }
+
+    public void createBars()
+    {
         //Create health mana and block bars
         healthBar = new Label(0,50);
         manaBar = new Label(0, 50);
         blockBar = new Label(0, 50);
-        
+
         addObject(healthBar, 50, 50);
-        addObject(manaBar, 100, 50);
-        addObject(blockBar, 150, 50);
-        
-        healthBar.setLineColor("#f71616"); 
-        
+        addObject(manaBar, 120, 50);
+        addObject(blockBar, 190, 50);
+
+        healthBar.setLineColor(healthColor); 
+        manaBar.setLineColor(manaColor);
+        blockBar.setLineColor(blockColor);
     }
-    
+
     public void act()
     {
         makeChoice();
+        updateBars();
+
+        if(fighting = true)
+        {
+            fight();
+            checkWinLose();
+        }
     }
     
+    public void fight()
+    {
+        if(myTurn = true)
+        {
+            if(Greenfoot.mouseClicked(attackButton))
+            {
+                turnDecision = 1;
+            }
+            
+            if(Greenfoot.mouseClicked(blockButton))
+            {
+                turnDecision = 3;
+            }
+            
+            if(Greenfoot.mouseClicked(skillButton))
+            {
+                turnDecision = 2;
+                removeObject(blockButton);
+                removeObject(attackButton);
+                removeObject(skillButton);
+                
+            }
+        }
+        System.out.println(turnDecision);
+            
+    }
+    
+    public void checkWinLose()
+    {
+        //if health bar of either side 0, win or lose screen :>
+    }
+
+    public void updateBars()
+    {
+        healthBar.setValue(health);
+        manaBar.setValue(mana);
+        blockBar.setValue(block);
+    }
+
     public void addEnemies()
     {
         addObject(enemy1, 300, 300);
     }
-    
+
     public void setChoice(boolean x)
     {
         choice = x;
@@ -89,7 +166,7 @@ public class MyWorld extends World
         GreenfootImage blueImg = choiceBlue.getImage();
         blueImg.scale(250, 250);
     }
-    
+
     public void makeChoice()
     {
         if(Greenfoot.mouseClicked(choicePink))
@@ -97,7 +174,7 @@ public class MyWorld extends World
             setChoice(true);
             createChar();
         }
-        
+
         if(Greenfoot.mouseClicked(choiceBlue))
         {
             setChoice(false);
@@ -132,18 +209,29 @@ public class MyWorld extends World
         //addObject(bat, 0,0);
 
     }
-    
+
     public void createFightWorld()
     {
-        //System.out.println("hiiii");
         addObject(fightWorld, 300, 200);
         GreenfootImage fightImg = fightWorld.getImage();
         fightImg.scale(600,410);
+        addFightStuff();
         
     }
-    
+
     public void addFightStuff()
     {
+        addObject(attackButton, 300, 280);
+        addObject(blockButton, 300,330);
+        addObject(skillButton, 300, 380);
         
+        GreenfootImage atkImage = attackButton.getImage();
+        GreenfootImage blkImage = blockButton.getImage();
+        GreenfootImage sklImage = skillButton.getImage();
+        
+        atkImage.scale(buttonWidth, buttonHeight);
+        blkImage.scale(buttonWidth, buttonHeight);
+        sklImage.scale(buttonWidth, buttonHeight);
+        fighting = true;
     }
 }
---
+++ SkillButton.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SkillButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SkillButton extends Actor
+{
+    /**
+     * Act - do whatever the SkillButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    MyWorld world = (MyWorld) getWorld();
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
